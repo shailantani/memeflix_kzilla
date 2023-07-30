f66b125f5212f0644b25ee3b37d19177e2cbd93d
shailantani <98448265+shailantani@users.noreply.github.com>
2023-07-31 02:19:42 +0530

    alpha

---
+++ lib/data/data.dart
@@ -0,0 +1,326 @@
+import 'package:flutter/material.dart';
+import 'package:memeflix/assets.dart';
+import 'package:memeflix/models/models.dart';
+
+final Content sintelContent = Content(
+  name: 'Sintel',
+  color: Colors.black,
+  imageUrl: Assets.sintel,
+  titleImageUrl: Assets.sintelTitle,
+  videoUrl:
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4',
+  description:
+      'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+);
+
+final List<Content> previews = [
+  Content(
+    name: 'Avatar The Last Airbender',
+    imageUrl: Assets.atla,
+    color: Colors.orange,
+    titleImageUrl: Assets.atlaTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'The Crown',
+    imageUrl: Assets.crown,
+    color: Colors.red,
+    titleImageUrl: Assets.crownTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'The Umbrella Academy',
+    imageUrl: Assets.umbrellaAcademy,
+    color: Colors.yellow,
+    titleImageUrl: Assets.umbrellaAcademyTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'Carole and Tuesday',
+    imageUrl: Assets.caroleAndTuesday,
+    color: Colors.lightBlueAccent,
+    titleImageUrl: Assets.caroleAndTuesdayTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'Black Mirror',
+    imageUrl: Assets.blackMirror,
+    color: Colors.green,
+    titleImageUrl: Assets.blackMirrorTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'Avatar The Last Airbender',
+    imageUrl: Assets.atla,
+    color: Colors.orange,
+    titleImageUrl: Assets.atlaTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'The Crown',
+    imageUrl: Assets.crown,
+    color: Colors.red,
+    titleImageUrl: Assets.crownTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'The Umbrella Academy',
+    imageUrl: Assets.umbrellaAcademy,
+    color: Colors.yellow,
+    titleImageUrl: Assets.umbrellaAcademyTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'Carole and Tuesday',
+    imageUrl: Assets.caroleAndTuesday,
+    color: Colors.lightBlueAccent,
+    titleImageUrl: Assets.caroleAndTuesdayTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+  Content(
+    name: 'Black Mirror',
+    imageUrl: Assets.blackMirror,
+    color: Colors.green,
+    titleImageUrl: Assets.blackMirrorTitle,
+    videoUrl: Assets.sintelVideoUrl,
+    description:
+        'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+  ),
+];
+
+final List<Content> myList = [
+  Content(
+      name: 'Violet Evergarden',
+      imageUrl: Assets.violetEvergarden,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'How to Sell Drugs Online (Fast)',
+      imageUrl: Assets.htsdof,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Kakegurui',
+      imageUrl: Assets.kakegurui,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Carole and Tuesday',
+      imageUrl: Assets.caroleAndTuesday,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Black Mirror',
+      imageUrl: Assets.blackMirror,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Violet Evergarden',
+      imageUrl: Assets.violetEvergarden,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'How to Sell Drugs Online (Fast)',
+      imageUrl: Assets.htsdof,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Kakegurui',
+      imageUrl: Assets.kakegurui,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Carole and Tuesday',
+      imageUrl: Assets.caroleAndTuesday,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Black Mirror',
+      imageUrl: Assets.blackMirror,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+];
+
+final List<Content> originals = [
+  Content(
+      name: 'Stranger Things',
+      imageUrl: Assets.strangerThings,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The Witcher',
+      imageUrl: Assets.witcher,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The Umbrella Academy',
+      imageUrl: Assets.umbrellaAcademy,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: '13 Reasons Why',
+      imageUrl: Assets.thirteenReasonsWhy,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The End of the F***ing World',
+      imageUrl: Assets.teotfw,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Stranger Things',
+      imageUrl: Assets.strangerThings,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The Witcher',
+      imageUrl: Assets.witcher,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The Umbrella Academy',
+      imageUrl: Assets.umbrellaAcademy,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: '13 Reasons Why',
+      imageUrl: Assets.thirteenReasonsWhy,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The End of the F***ing World',
+      imageUrl: Assets.teotfw,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+];
+
+final List<Content> trending = [
+  Content(
+      name: 'Explained',
+      imageUrl: Assets.explained,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Avatar The Last Airbender',
+      imageUrl: Assets.atla,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Tiger King',
+      imageUrl: Assets.tigerKing,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The Crown',
+      imageUrl: Assets.crown,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Dogs',
+      imageUrl: Assets.dogs,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Explained',
+      imageUrl: Assets.explained,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Avatar The Last Airbender',
+      imageUrl: Assets.atla,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Tiger King',
+      imageUrl: Assets.tigerKing,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'The Crown',
+      imageUrl: Assets.crown,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+  Content(
+      name: 'Dogs',
+      imageUrl: Assets.dogs,
+      titleImageUrl: Assets.sintelTitle,
+      videoUrl: Assets.sintelVideoUrl,
+      description: "lol",
+      color: Colors.black),
+];
