c448abdc6199b71859797494b3bebec689af8c51
shailantani <98448265+shailantani@users.noreply.github.com>
2023-07-31 09:11:18 +0530

    alpha

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/shailantani/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/shailantani/new flutter/flutter_netflix_responsive_ui-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/shailantani/new flutter/flutter_netflix_responsive_ui-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lMTRkYjY4YTg2MTk5YTlmYTVmZDk2MjhkOGUyOTBhYWZjMGY2OTE3Lw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/shailantani/new flutter/flutter_netflix_responsive_ui-master/.dart_tool/package_config.json"
